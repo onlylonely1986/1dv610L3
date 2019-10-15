ef50c562dba45b4e23d8e9308ce7bc69dc3e9e77
Lone Nilsson <onlylonely1986@gmail.com>
2019-10-15 20:01:50 +0200

    creating files

---
+++ RunApplication.php
@@ -0,0 +1,13 @@
+<?php
+
+class RunApplication {
+
+    public function __construct() {
+        // MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
+        error_reporting(E_ALL);
+        ini_set('display_errors', 'On');
+
+        require_once('controller/MainController.php');
+        $c = new MainController();
+    }
+}
--- index.php
+++ index.php
@@ -1,18 +1,5 @@
 <?php
 
-// MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
-error_reporting(E_ALL);
-ini_set('display_errors', 'On');
+require_once('RunApplication.php');
 
-
-require_once('view/LayoutView.php');
-require_once('view/LoginView.php');
-require_once('view/PicsView.php');
-
-
-$lv = new LayoutView();
-$v = new LoginView();
-$pv = new PicsView();
-
-$lv->render($v, $pv);
-
+$start= new RunApplication();
